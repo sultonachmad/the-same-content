8c7bc8946d33b189e0dbe47270475cddcf11ba39
Sulthan Achmad <sultonachmad@gmail.com>
2022-03-14 22:23:45 +0700

    add required to fileupload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <form method="post" action="process.php"  enctype="multipart/form-data">
-<input type="file" name="file_upload[]" multiple="multiple"  webkitdirectory mozdirectory />
+<input type="file" name="file_upload[]" required multiple="multiple"  webkitdirectory mozdirectory />
 <br/>
 <input type="submit" value="submit" />
 </form>
