26cf7e37b916d10815d8a0da8937622c52b670f0
Sulthan Achmad <sultonachmad@gmail.com>
2022-03-14 21:56:45 +0700

    upload folder view

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+<form method="post" action="process.php"  enctype="multipart/form-data">
+<input type="file" name="file_upload[]" multiple="multiple"  webkitdirectory mozdirectory />
+<br/>
+<input type="submit" value="submit" />
+</form>
+</html>
